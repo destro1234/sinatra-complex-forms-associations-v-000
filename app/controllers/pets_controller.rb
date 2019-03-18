dec17d71c48d57cde75f8faeadaacacad75f5c58
Elias Ventura <eliasventura23@gmail.com>
2019-03-18 14:27:20 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -30,6 +30,11 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pets.owner = Owner.create(name: params["owner"]["name"])
+    end
 
     redirect to "pets/#{@pet.id}"
   end
